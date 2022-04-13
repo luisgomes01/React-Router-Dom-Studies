b98cb4d57c27c3375b4caf1a2ab414f2ad43cebd
Luís Gomes <luis.gomes@liferay.com>
2022-04-13 09:40:12 -0300

    useLocation() - React Router DOM

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,35 @@
-import { MAIN_SECTION_TEXT, MAIN_SECTION_TEXT2 } from "../utils/constants";
-import { useParams, useNavigate } from "react-router-dom";
+import {
+  MAIN_SECTION_TEXT,
+  MAIN_SECTION_TEXT2,
+  NOT_FOUND_TEXT,
+} from "../utils/constants";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 
 export default function Portfolio() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  console.log(location);
+
+  function generateIdAndNavigate() {
+    const generatedId = Math.floor(Math.random() * 11);
+    navigate(`${generatedId}`);
+  }
+
+  if (location.pathname === "/portfolio") {
+    return (
+      <div id="what-the-hell-is-this">
+        <div className="page-title">
+          <h2>
+            <h1>Make sure the url has an ID</h1>
+          </h2>
+          <p className="lead">{NOT_FOUND_TEXT}</p>
+        </div>
+        <button onClick={() => generateIdAndNavigate()}>Add ID</button>
+      </div>
+    );
+  }
 
   return (
     <div id="what-the-hell-is-this">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,6 @@ export const MAIN_SECTION_TEXT =
 
 export const MAIN_SECTION_TEXT2 =
   "that can be easily reused in both mobile and native displays. Mouseover the nav text for animation!";
+
+export const NOT_FOUND_TEXT =
+  "Click on the button to add an ID to the url to continue";

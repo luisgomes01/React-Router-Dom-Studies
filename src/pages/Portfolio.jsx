e5ce0fe5be63d402a455aae730780f799b2d1769
Luís Gomes <luis.gomes@liferay.com>
2022-04-13 08:51:41 -0300

    useNavigate() - React Router DOM

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,10 @@
 import { MAIN_SECTION_TEXT, MAIN_SECTION_TEXT2 } from "../utils/constants";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+
 export default function Portfolio() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   return (
     <div id="what-the-hell-is-this">
       <div className="page-title">
@@ -12,6 +15,7 @@ export default function Portfolio() {
           {MAIN_SECTION_TEXT2}
         </p>
       </div>
+      <button onClick={() => navigate("/")}>Head home</button>
     </div>
   );
 }
